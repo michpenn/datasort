83576a994876c2fc4d12053d02b29f90a55bc64a
michpenn <michpenn91@gmail.com>
2018-11-19 17:49:46 -0500

    initial commit of datasort files

--- README.md
+++ README.md
@@ -1,3 +1,43 @@
+# Datasort
+
+Datasort takes in a CSV and sorts the data. In this case, I have used a CSV containing data from Game Of Thrones.
+
+## Requirements
+
+- Create a view that displays entries from the dataset as blocks, squares, or rows with the
+  name visible
+- If there are any empty results with any of the user interactions, please display an
+  appropriate error message (preferably a Sean Bean reference)
+- When the user clicks on an entry, enable the user to view additional available information
+  about the entry in a separate view on the same page:
+  - Title = title
+  - Gender = isMale
+  - Culture = culture
+  - DOB = dateOfBirth
+- Each of the below user-abilities can be reset by a hard refresh of the browser by the user
+  - Enable the user the ability to show all entries in no particular order
+  - Enable the user the ability to show the entries by its corresponding isAlive value
+  - Enable the user the ability to show the entries by their popularity in descending
+    order
+
+## Extra Credit
+
+- Use Ag-grid to display the entries
+- Share your project in a git/svn repository
+- Enable the user to reset the view back to the initially loaded view to avoid a hard refresh
+- Enable random order of entries when initially loaded
+- Incorporate deferred loading to scroll more to replace the ability to show all
+- Enable the user to show the entries by their popularity in ascending order as well
+- Enable the user to display an area that has a pie chart of the proportional genders of the
+  entries
+
+### Potential Future Implementations
+
+- Due to the time constraint, I used an upload CSV feature rather than a CSV parser. I would like to implement that the data is automatically loaded from the CSV.
+- To enable the refresh of the table and the random order features, I would implement the [Refresh and Redraw functionality](https://www.ag-grid.com/javascript-grid-refresh/) of Ag-grid
+- To enable the deferred loading features, I would implement the [Infinite Scrolling](https://www.ag-grid.com/javascript-grid-infinite-scrolling/) feature of Ag-grid. However, by implementing the Infinite Scrolling feature, it is not possible to sort or filter the data as it is not all present.
+- To enable the pie chart feature, I would use a JS library, like D3. An example of Ag-grid using external graphing can be found [here](https://www.ag-grid.com/javascript-grid-graphing/)
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,25 +1,157 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import ReactFileReader from "react-file-reader";
+import Papa from "papaparse";
+import { AgGridReact } from "ag-grid-react";
+
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-balham.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gridOptions: {
+        rowSelection: "single",
+        enableFilter: true,
+        columnDefs: [
+          {
+            headerName: "Name",
+            field: "name",
+            width: 200,
+            suppressFilter: true
+          },
+          {
+            headerName: "House",
+            field: "house",
+            width: 200,
+            suppressFilter: true
+          },
+          {
+            headerName: "Is Alive",
+            field: "isAlive",
+            width: 200,
+            filter: "agTextColumnFilter",
+            filterParams: {
+              apply: true,
+              applyButton: true,
+              clearButton: true
+            }
+          },
+          {
+            headerName: "Popularity %",
+            field: "popularity",
+            width: 200,
+            suppressFilter: true,
+            unSortIcon: true
+          }
+        ]
+      },
+      rowData: [],
+      selected: {}
+    };
+  }
+
+  handleFiles = files => {
+    console.log(files[0]);
+    Papa.parse(files[0], {
+      complete: (results, file) => {
+        console.log(results);
+        this.setState({ rowData: results.data });
+      },
+      header: true,
+      skipEmptyLines: true,
+      transform: (value, col) => {
+        if (value === "") {
+          switch (col) {
+            case "house":
+              return "In this world only winter is certain";
+            case "title":
+              return "Black and white and grey, all the shades of truth";
+            case "dateOfBirth":
+              return "Some secrets are safer kept hidden";
+            case "culture":
+              return "I've made many mistakes in my life, but that wasn't one of them.";
+            default:
+              return "I will have no part in it.";
+          }
+        } else if (col === "isMale") {
+          if (value === "0") {
+            return "Female";
+          }
+          return "male";
+        } else if (col === "isAlive") {
+          if (value === "0") {
+            return "False";
+          }
+          return "True";
+        } else if (col === "popularity") {
+          return parseFloat(value) * 100;
+        }
+        return value;
+      }
+    });
+  };
+
+  onGridReady = params => {
+    this.gridApi = params.api;
+    this.gridColumnApi = params.columniApi;
+  };
+
+  onSelectionChanged = () => {
+    const selected = this.gridApi.getSelectedRows();
+    this.setState({ selected: selected[0] });
+  };
+
   render() {
+    const selected = this.state.selected.title ? (
+      <ul style={{ textAlign: "left" }}>
+        <li>Title: {this.state.selected.title}</li>
+        <li>Gender: {this.state.selected.isMale}</li>
+        <li>Culture: {this.state.selected.culture}</li>
+        <li>DOB: {this.state.selected.dateOfBirth}</li>
+      </ul>
+    ) : null;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div style={{ width: "100%", height: "100vh" }}>
+          <div>
+            <ReactFileReader
+              fileTypes={[".csv"]}
+              handleFiles={this.handleFiles}
+            >
+              <button className="btn">Upload</button>
+            </ReactFileReader>
+          </div>
+          <div style={{ marginTop: "15px", height: "100%", display: "flex" }}>
+            <div
+              id="myGrid"
+              style={{
+                boxSizing: "border-box",
+                width: "70%",
+                height: "100%"
+              }}
+              className="ag-theme-balham"
+            >
+              <AgGridReact
+                onGridReady={this.onGridReady}
+                gridOptions={this.state.gridOptions}
+                rowData={this.state.rowData}
+                rowSelection={this.state.rowSelection}
+                onSelectionChanged={this.onSelectionChanged.bind(this)}
+                enableSorting={true}
+              />
+            </div>
+            <div
+              style={{
+                height: "100%"
+              }}
+            >
+              <div className="ag-theme-balham">{selected}</div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
